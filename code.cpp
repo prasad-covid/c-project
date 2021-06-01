fa3e05743b6952da01fccc74f5983d7e1f8160f5
prasad-covid <63596951+prasad-covid@users.noreply.github.com>
2021-06-01 20:29:26 +0530

    created code.cpp

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float a,b,c;
+	cout<<"Enter two values: ";
+	cin>>a;
+	cin>>b;
+	c=(a*b);
+	cout<<"The product of two numbers is: "<<c<<endl;
+	return 0;
+}
